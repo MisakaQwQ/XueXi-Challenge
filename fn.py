e1059a3a49ea6d0485d5595977febe452f106ef1
MisakaQwQ <charqyq@gmail.com>
2022-07-18 11:16:56 +0800

    gpu pytorch

--- fn.py
+++ fn.py
@@ -18,7 +18,7 @@ import difflib
 
 all_questions = []
 question_text = []
-ocr = CnOcr()
+ocr = CnOcr(context='gpu', model_backend='pytorch')
 
 
 def init():
@@ -57,7 +57,7 @@ class OCR_backend(QThread):
         self.mutex = QMutex()
         self.cond = QWaitCondition()
 
-    # @timer
+    @timer
     def search_db(self):
         def builder(font_size, color, font_family, content):
             html = '''<span style="font-size:%dpx;color:%s;font-family:'%s';"> %s <br /><br /></span>''' \
@@ -84,7 +84,7 @@ class OCR_backend(QThread):
                    'p, li { white-space: pre-wrap; }</style></head><body> %s </body></html>' % html
         self.update_ans.emit(end_html)
 
-    # @timer
+    @timer
     def process(self):
         # 二值化
         ret, binary = cv2.threshold(self.image, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
@@ -211,7 +211,7 @@ class Main_backend(QThread):
         wrapped_image = image[left_up[1]:right_bot[1] + 1, left_up[0]:right_bot[0] + 1]
         return wrapped_image
 
-    # @timer
+    @timer
     def image_process(self, image):
         wrapped_image = self.image_wrap(image)
         if self.mode == 1:
